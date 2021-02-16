fd7a54e40af8a834f69d21e22b0e0cd3e55ddae4
Michel Vernier <michel.vernier@openvalue.fr>
2021-02-16 11:04:50 +0100

    Commit fin tuto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Michel VERNIER !'));
+app.get('/', (req, res) => res.send('Hello Michel VERNIER, FIN DU PIPELINE !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
